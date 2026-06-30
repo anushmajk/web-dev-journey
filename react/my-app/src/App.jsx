703e50a090a7b6f3d1356453786e52404f768a0b
Anushma JK <anushmajk@gmail.com>
2026-06-30 10:23:32 +0530

    Add initial implementation of Home and Header components with styling

--- react/my-app/src/App.jsx
+++ react/my-app/src/App.jsx
@@ -75,4 +75,34 @@ const App = () => {
   );
 }
 
-export default App
+export default App 
+/*import axios from 'axios';
+import { Routes, Route } from 'react-router';
+import { useState, useEffect } from 'react';
+import { Home } from './pages/Home';
+import { CheckoutPage } from './pages/checkout/CheckoutPage';
+import { OrdersPage } from './pages/orders/OrdersPage';
+import './App.css'
+
+function App() {
+  const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    const fetchAppData = async () => {
+      const response = await axios.get('/api/cart-items?expand=product');
+      setCart(response.data);
+    };
+
+    fetchAppData();
+  }, []);
+
+  return (
+    <Routes>
+      <Route index element={<HomePage cart={cart} />} />
+      <Route path="checkout" element={<CheckoutPage cart={cart} />} />
+      <Route path="orders" element={<OrdersPage cart={cart} />} />
+    </Routes>
+  )
+}
+
+export default App */
