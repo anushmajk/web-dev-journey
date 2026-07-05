7943b06d13e55ed7235812af66a4455e9eb0229e
Anushma JK <148621972+anushmajk@users.noreply.github.com>
2026-07-05 06:47:27 +0000

    create multiple pages with router

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import { HomePage } from './pages/HomePage'
 import { Routes, Route } from 'react-router'
 
+
 function App() {
   return (
     <>
--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import './header.css';
 import './HomePage.css';
 
+
 export function HomePage(){
     return (
         <>
