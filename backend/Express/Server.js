2283dda9cc209a9952a7cc7bec3b18716883a13b
Anushma JK <148621972+anushmajk@users.noreply.github.com>
2026-07-27 06:17:00 +0000

    .changes made by the user.

---
+++ backend/Express/Server.js
@@ -0,0 +1,32 @@
+/*const express =require('express');
+const app=express();
+app.get('/',(req,res)=>{
+    res.send('Hello World');
+});
+const PORT =3000;
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+});*/
+
+const express =require('express');
+const app=express();
+app.use(express.json());
+app.post("/login",(req,res)=>{
+
+const {username,password}=req.body;
+
+if(username==="admin" && password==="1234"){
+    res.json({
+        message:"Login Successful"
+    });
+}
+else{
+    res.status(401).json({
+        message:"Invalid Credentials"
+    });
+}
+});
+const PORT =3000;
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+});
